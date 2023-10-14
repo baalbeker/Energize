faee85aaf7fc28c4f3d2dcbb51c8874e7595dfbe
samuilYoshkov <magmatogorsk@protonmail.com>
2023-10-14 19:43:34 +0300

    add minor changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import {getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getStorage} from "firebase/storage"
 
 
-//turin profile
 const firebaseConfig = {
-  apiKey: "AIzaSyDVZEjFbpPWKYsGkxJcvRW4pUlNKTqsaM4",
-  authDomain: "tracker-46d44.firebaseapp.com",
-  projectId: "tracker-46d44",
-  storageBucket: "tracker-46d44.appspot.com",
-  messagingSenderId: "707139540702",
-  appId: "1:707139540702:web:6debbd0f5d6e772b77944f"
+  apiKey: "AIzaSyAa0wafio-lw74IU6--uKT9urs0MVl-tAg",
+  authDomain: "fitnesstracker-47280.firebaseapp.com",
+  projectId: "fitnesstracker-47280",
+  storageBucket: "fitnesstracker-47280.appspot.com",
+  messagingSenderId: "704745838903",
+  appId: "1:704745838903:web:6e749a718beeba263ce2b2",
+  measurementId: "G-4J5BJ90QGR"
 };
 
 // Initialize Firebase
